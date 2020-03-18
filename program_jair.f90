9fb320325bf83dd908bb46e4e5c9417fda11978c
marcospiza <marcosalex.ma@gmail.com>
2020-03-18 20:21:05 -0300

    correcao do codigo e do arquivo de entrada

--- program_jair.f90
+++ program_jair.f90
@@ -1,7 +1,7 @@
 program pibic2
 implicit none
-real,dimension(:),allocatable:: TMAX,TMED,TMIN,LAT, HORA,UR,VEL,LT
-integer,dimension(:),allocatable::ANO, MES, DIA
+real,dimension(:),allocatable:: TMAX,TMED,TMIN, HORA,UR,VEL,LT  !!!
+integer,dimension(:),allocatable::ANO, MES, DIA  ! Tem q ser integer porque são parametros da funcao DJ e sao integers
 real,parameter:: pi = 3.1415927
 real,parameter:: Gsc = 0.0820
 real,parameter:: as = 0.25
@@ -9,11 +9,13 @@ real,parameter:: bs = 0.5
 real,parameter:: cstefan = 0.000004903
 REAL,PARAMETER:: Y = 0.063
 Character DTA*12 ! data
-integer DJ ! dia juliano
+!integer DJ ! dia juliano >>> === Erro: deletar. DJ já é nome de função. Ta havendo conflito
+INTEGER DJi  ! Representa o valor de DJ para cada iteração I
 INTEGER I
 INTEGER N_dados
 integer DM ! dia mes
 REAL BIX
+REAL LAT  ! LAT = Latitude tem q ser escalar e  nao vetor
 REAL T
 REAL ESTMAX
 REAL ESTMIN
@@ -45,13 +47,21 @@ do I = 1,N_dados
    ES= ( ESTMAX + ESTMIN ) / 2.
    S = ( 4098 * ES ) / ( ( T + 237.3 ) **2 )
    EA = ( UR(I) * ES ) / 100.
-   G = TMAX(I) - TMIN(I)
-   D = ( 0.409 *SIN ( ( ( PI * 2 / 365. ) * DJ(DM, MES, ANO) ) - 1.39 ) ) ! Radiano
+   G = TMAX(I) - TMIN(I) ! Aqui ta estrnho.
+ ! G é o fluxo de calor do solo??? Se for, esse calculo esta errado
+
+
+ ! OBS Não tinha valor atribuido para DM
+ ! MES e ANO são vetores. Deve-se pegar o valor do vetor para cada iteração. Ou seja: MES(I), ANO(I)
+ ! Eu modifiquei para USAR o DJi, porqur assim a funcão DJ só é chamada uma vez
+   DJi = DJ(DIA(I), MES(I), ANO(I))
+   DM = DIA(I) ! Valor do dia do mes
+   D = ( 0.409 *SIN ( ( ( PI * 2 / 365. ) * DJi ) - 1.39 ) ) ! Radiano
    
    
    ! Distancia relativa inversa Terra-Sol
    
-   Dr = 1 + ( 0.033 * ( cos ( 2 * pi / 365. * DJ(DM, MES, ANO) ) ) )
+   Dr = 1 + ( 0.033 * ( cos ( 2 * pi / 365. * DJi ) ) ) ! Mesmo erro acima. Usei DJi
    
    ! latitude
    
@@ -59,14 +69,14 @@ do I = 1,N_dados
    
    ! angulo da hora do por do sol
    
-   WS= ACOS ( - TAN ( LAT ) * TAN ( D ) )
+   WS= ACOS ( - TAN ( LAT ) * TAN ( D ) ) !! Aqui tinha um erro: o era pq LAT estava como vetor
    
    ! Radiacao extraterrestre por periodos diarios
    
    RADC = (( 24 * 60 ) / PI ) * Gsc  * DR  * ((( WS * SIN ( LAT ) * SIN ( D )) + ( COS ( LAT ) * COS ( D ) * SIN ( WS ))))
    
    ! Calcular radiacao solar
-   NZ = HORA(I) / DM
+   NZ = HORA(I) / DM  ! DM foi calculado acima
    NE = ( 24 / PI ) * WS
    RS = ( ( AS  + BS ) * ( NE / NZ ) ) * RADC
    !calcular radiacao solar em ceu claro
@@ -79,6 +89,8 @@ do I = 1,N_dados
    
    
    ETP = (((0.408 * S * (RNL - G)) + ((Y * 900 * VEL(I) * (ES - EA)) / (T + 273))) / (S + (Y *( 1+ 0.34) * VEL(I))))
+   print*, ETP
+!  Note que tem um valor negativo de ETP. Verificar o porque?
    
 
 enddo
@@ -101,6 +113,7 @@ OPEN(1,FILE=dd,STATUS="OLD")
 
 READ(1,*)
 
+
 N=0
 DO WHILE (IO == 0)
    READ (1, *, IOSTAT = IO)
@@ -112,7 +125,6 @@ Nout = N
 
 CLOSE(1)
 
-
 ALLOCATE(TMAX(N))
 ALLOCATE(TMIN(N))
 ALLOCATE(LT(N))
@@ -121,24 +133,33 @@ ALLOCATE(TMED(N))
 ALLOCATE(UR(N))
 ALLOCATE(VEL(N))
 
-OPEN(1,FILE="datar",STATUS="OLD")
+!! Faltou alocar: ANO, MES e DIA
+
+ALLOCATE(ANO(N))
+ALLOCATE(MES(N))
+ALLOCATE(DIA(N))
+
+! Erro aqui. Deve colocar a extensao do arquivo: datar.txt.
+! Melhor colocar a variavel dd, q ja tem esse nome armazenado
+!OPEN(1,FILE="datar",STATUS="OLD")
+OPEN(1,FILE=dd,STATUS="OLD")
 
 READ(1,*)
 
-OPEN(2,FILE="sds.txt",STATUS="replace")
 
+!OPEN(2,FILE="sds.txt",STATUS="replace") !!!! Para que abir esse arquivo????
 DO I=1,N
-
    READ(1,*)DTA,TMAX(I),TMIN(I),LT(I), HORA(I), TMED(I), UR(I), VEL(I)
-    
-   READ(DTA(7:12),"(I4)") ANO(I)
+!!! ERRO >>> A conversão estava errada. O formato da data no arquivo é: 30-03-2016
+!!! Logo, o ANO é do 7 ao 10.
+   READ(DTA(7:10),"(I4)") ANO(I)
    
    READ(DTA(4:5),"(I2)") MES(I)
    
    READ(DTA(1:2),"(I2)") DIA(I)
    
-
 ENDDO
+
 
 CLOSE(1)
 
@@ -146,7 +167,7 @@ CLOSE(1)
 END SUBROUTINE
 
 function DJ(DM,MES,ANO)
-integer,intent(out):: DJ
+integer::  DJ  ! Nome de função não requer intent(out). Por padrao, tem q ser out
 integer,intent(in):: DM, MES, ANO
 
 bix = mod (ANO, 4) !Condição para definir se o ano e bissexto ou não da divisão (ano/4)
